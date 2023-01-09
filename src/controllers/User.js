38833a752594d8ea8f87a902a2f4f5986cf8a6c1
Gabriel Luiz Carpes <gabrielluizcm@hotmail.com>
2023-01-09 11:21:08 -0300

    CRUD routes for users

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -9,6 +9,56 @@ class UserController {
       return res.status(400).json({ errors: e.errors.map((err) => err.message) });
     }
   }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (error) {
+      return res.status(500).json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      if (!req.params.id) { return res.status(400).json({ errors: ['ID is missing'] }); }
+
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      return res.status(500).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) { return res.status(400).json({ errors: ['ID is missing'] }); }
+
+      const user = await User.findByPk(req.params.id);
+      if (!user) { return res.status(400).json({ errors: ['User not found'] }); }
+
+      const updatedUser = await user.update(req.body);
+
+      return res.json(updatedUser);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) { return res.status(400).json({ errors: ['ID is missing'] }); }
+
+      const user = await User.findByPk(req.params.id);
+      if (!user) { return res.status(400).json({ errors: ['User not found'] }); }
+
+      await user.destroy();
+
+      return res.json(null);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
 }
 
 export default new UserController();
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,10 @@ import UserController from '../controllers/User';
 
 const router = new Router();
 
+router.get('/', UserController.index);
+router.get('/:id', UserController.show);
 router.post('/', UserController.create);
+router.put('/:id', UserController.update);
+router.delete('/:id', UserController.delete);
 
 export default router;
