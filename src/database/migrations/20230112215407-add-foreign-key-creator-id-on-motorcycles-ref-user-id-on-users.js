29c31e43150ccefe6ff406c7f6e9d7516104edcb
Gabriel Luiz <dev.gabrielluiz@gmail.com>
2023-01-12 18:58:34 -0300

    Fixed migrations

--- src/database/migrations/20230112215407-add-foreign-key-creator-id-on-motorcycles-ref-user-id-on-users.js
+++ src/database/migrations/20230112215407-add-foreign-key-creator-id-on-motorcycles-ref-user-id-on-users.js
@@ -3,7 +3,11 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.changeColumn('Motorcycles', 'creator_id', {
       type: Sequelize.INTEGER,
+      defaultValue: null,
       allowNull: true,
+    });
+    await queryInterface.changeColumn('Motorcycles', 'creator_id', {
+      type: Sequelize.INTEGER,
       references: {
         model: 'Users',
         key: 'id',
